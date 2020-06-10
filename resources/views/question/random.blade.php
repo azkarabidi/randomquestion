c8b039af1e31e6494935bf92760efb73c828ae50
Azkar Abidi <Azkarneedforspeed@gmail.com>
2020-06-10 16:11:55 +0800

    add sweet alert

--- resources/views/question/random.blade.php
+++ resources/views/question/random.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/8.11.8/sweetalert2.min.css" integrity="sha256-2bAj1LMT7CXUYUwuEnqqooPb1W0Sw0uKMsqNH0HwMa4=" crossorigin="anonymous" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/8.11.8/sweetalert2.all.min.js" integrity="sha256-2RS1U6UNZdLS0Bc9z2vsvV4yLIbJNKxyA4mrx5uossk=" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body>
@@ -14,12 +16,44 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script>
         $('#random').click(function(){
-            $.get("{{route('question')}}")
+            // $.get("{{route('question')}}")
+            // .done(function(data){
+            //     alert(data.id);
+            //     console.log(data);
+            // })
+
+let timerInterval
+Swal.fire({
+  title: 'Auto picking qustion',
+  html: 'Please Wait',
+  timer: 1500,
+  timerProgressBar: true,
+  onBeforeOpen: () => {
+    Swal.showLoading()
+    timerInterval = setInterval(() => {
+      const content = Swal.getContent()
+      if (content) {
+        const b = content.querySelector('b')
+        if (b) {
+          b.textContent = Swal.getTimerLeft()
+        }
+      }
+    }, 100)
+  },
+  onClose: () => {
+    clearInterval(timerInterval)
+  }
+}).then((result) => {
+  /* Read more about handling dismissals below */
+  if (result.dismiss === Swal.DismissReason.timer) {
+    $.get("{{route('question')}}")
             .done(function(data){
-                alert(data.id);
-                console.log(data);
+    Swal.fire(`${data.question}`);
             })
-        })
+  }
+})
+})  
+    
     </script>
 </body>
 </html>
